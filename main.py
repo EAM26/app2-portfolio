f5c978ffe8c37295d39f0436622de349caa7db56
EAM26 <emuller2607@gmail.com>
2024-08-26 12:32:41 +0200

    Create columns and basic content homepage

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/my_photo.png")
+
+with col2:
+    st.title("Emile Muller")
+    content = """
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra arcu tristique
+    magna aenean taciti. Tellus nunc gravida, curabitur metus tempor 
+    himenaeos! Enim duis leo dolor senectus nullam dolor fusce consectetur. 
+    Ornare at fusce augue litora; eget praesent. Vestibulum nec tincidunt id 
+    habitasse faucibus hac himenaeos; pharetra etiam. Lacinia class porttitor 
+    rutrum fusce urna etiam. Aliquet hendrerit platea cubilia mollis mus 
+    vitae orci.\n
+    Lacus condimentum eros hendrerit aliquam habitasse ut parturient. Per
+    eleifend ultrices sollicitudin cursus tempor nostra vestibulum id. 
+    Penatibus lacus quisque class commodo suspendisse. Bibendum torquent 
+    turpis viverra pellentesque tortor diam ultrices. Maximus luctus leo 
+    eleifend est donec phasellus. Tincidunt egestas purus vulputate 
+    scelerisque justo risus. Placerat cras finibus augue adipiscing pharetra 
+    netus semper tempor. Vulputate tempus feugiat massa eleifend libero 
+    ridiculus. Euismod dictum ullamcorper enim tellus ex netus.
+    """
+    st.info(content)
