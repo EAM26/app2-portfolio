573f930f18d0cfe67b7b89130578619d2248498d
EAM26 <emuller2607@gmail.com>
2024-08-27 13:52:20 +0200

    Add under construction image to homepage

--- Home.py
+++ Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
 
 content_intro = """
     Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra arcu tristique
@@ -27,6 +26,16 @@ Hieronder vindt u enkele van de apps die ik heb ontwikkeld. Neem gerust
 contact met mij op!
 """
 
+col_empty_1, col_image_under_const, col_empty_2 = st.columns([2, 3, 1])
+with col_image_under_const:
+    st.image("images/under-construction.png", caption='Website is under '
+                                                      'construction')
+
+st.markdown('<div style="margin: 120px 0;"></div>',
+            unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+
 with col1:
     st.image("images/my_photo.png")
 
@@ -39,19 +48,17 @@ st.write(contact_message)
 col3, empty_col, col4 = st.columns([1.5, .5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
+
 with col3:
-    for index, row in df[:int(len(df)/2)].iterrows():
+    for index, row in df[:int(len(df) / 2)].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image(f"images/{row['image']}")
         st.write(f"[Source code]({row['url']})")
-
 
 with col4:
-    for index, row in df[int(len(df)/2):].iterrows():
+    for index, row in df[int(len(df) / 2):].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image(f"images/{row['image']}")
         st.write(f"[Source code]({row['url']})")
-
-
