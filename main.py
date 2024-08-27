729603442e4c2c404bd021049acb093c441bb331
EAM26 <emuller2607@gmail.com>
2024-08-27 11:34:42 +0200

    Add app info, links and images with pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -34,3 +35,23 @@ with col2:
     st.info(content_intro)
 
 st.write(contact_message)
+
+col3, empty_col, col4 = st.columns([1.5, .5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:int(len(df)/2)].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source code]({row['url']})")
+
+
+with col4:
+    for index, row in df[int(len(df)/2):].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source code]({row['url']})")
+
+
