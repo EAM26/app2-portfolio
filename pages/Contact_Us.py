f888c5117ee705b01a567461e99dfd0e455251c7
EAM26 <emuller2607@gmail.com>
2024-08-28 13:10:06 +0200

    Add Contact us page with email functionality

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,20 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact me ")
+
+with st.form(clear_on_submit=True, key="email_forms"):
+    user_email = st.text_input("Your email address")
+    raw_message = st.text_area("Your message")
+
+    # Use backslash, blank rule and no indentations for correct format message
+    message = f"""\
+Subject: New mail from {user_email} 
+    
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button and message != "":
+        send_email(message)
+        st.info("Your mail has been sent.")
