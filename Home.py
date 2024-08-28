056f267cce3146080f92c439ccf39b309c15ac1f
EAM26 <emuller2607@gmail.com>
2024-08-28 18:13:27 +0200

    Add sample text and create function to show columns

--- Home.py
+++ Home.py
@@ -1,30 +1,22 @@
 import streamlit as st
 import pandas
 
+
+def show_app_column(data_row):
+    st.header(data_row['title'])
+    st.write(data_row['description'])
+    st.image(f"images/{data_row['image']}")
+    st.write(f"[Source code]({data_row['url']})")
+
+
 st.set_page_config(layout="wide")
 
-content_intro = """
-    Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra arcu tristique
-    magna aenean taciti. Tellus nunc gravida, curabitur metus tempor 
-    himenaeos! Enim duis leo dolor senectus nullam dolor fusce consectetur. 
-    Ornare at fusce augue litora; eget praesent. Vestibulum nec tincidunt id 
-    habitasse faucibus hac himenaeos; pharetra etiam. Lacinia class porttitor 
-    rutrum fusce urna etiam. Aliquet hendrerit platea cubilia mollis mus 
-    vitae orci.\n
-    Lacus condimentum eros hendrerit aliquam habitasse ut parturient. Per
-    eleifend ultrices sollicitudin cursus tempor nostra vestibulum id. 
-    Penatibus lacus quisque class commodo suspendisse. Bibendum torquent 
-    turpis viverra pellentesque tortor diam ultrices. Maximus luctus leo 
-    eleifend est donec phasellus. Tincidunt egestas purus vulputate 
-    scelerisque justo risus. Placerat cras finibus augue adipiscing pharetra 
-    netus semper tempor. Vulputate tempus feugiat massa eleifend libero 
-    ridiculus. Euismod dictum ullamcorper enim tellus ex netus.
-    """
+with open("sample_text.txt") as file:
+    sample_text = file.read()
 
-contact_message = """
-Hieronder vindt u enkele van de apps die ik heb ontwikkeld. Neem gerust 
-contact met mij op!
-"""
+content_intro = sample_text[:1000]
+
+contact_message = sample_text[:200]
 
 col_empty_1, col_image_under_const, col_empty_2 = st.columns([2, 3, 1])
 with col_image_under_const:
@@ -51,14 +43,9 @@ df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:int(len(df) / 2)].iterrows():
-        st.header(row['title'])
-        st.write(row['description'])
-        st.image(f"images/{row['image']}")
-        st.write(f"[Source code]({row['url']})")
+        show_app_column(row)
 
 with col4:
     for index, row in df[int(len(df) / 2):].iterrows():
-        st.header(row['title'])
-        st.write(row['description'])
-        st.image(f"images/{row['image']}")
-        st.write(f"[Source code]({row['url']})")
+        show_app_column(row)
+        
